57fd09a0e57df6ab7ac88b35fffdc2050811ad2d
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2022-09-24 18:00:04 +0300

    devspace sync

--- devspace/main.cpp
+++ devspace/main.cpp
@@ -1,11 +1,9 @@
 ﻿
-import libksn.window;
 import libksn.time;
-
-#pragma comment(lib, "Onecore.lib")
 
 int main()
 {
-	ksn::init_hybrid_sleep_threshold();
-	ksn::window_t win;
+
+	ksn::now();
+
 }
